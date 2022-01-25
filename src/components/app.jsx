6a2bbd0cd18f497b4c55581f1a97e9a6f6c658c0
Aditya Mishra <svmaditya01@gmail.com>
2022-01-25 21:11:10 +0530

    Deleted useless files

---
+++ src/components/app.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "./header.jsx";
+import Footer from "./footer.jsx";
+import Content from "./content.jsx";
+
+function App(){
+    return <div> Hello World</div>
+}
+
+export default App;
